890feec92b68b05288ba3a72de81b2ddaf7c809d
Alvian14 <cidukgaming03@gmail.com>
2025-06-23 18:32:11 +0700

    commit final done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>OrganizME</title>
-    {{-- @viteReactRefresh
-    @vite(['resources/css/custom.css', 'resources/js/main.jsx']) --}}
+    @viteReactRefresh
+    @vite(['resources/css/custom.css', 'resources/js/main.jsx'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
